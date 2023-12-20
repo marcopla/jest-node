53a890188c1243c0128bd59f0acfa2f1372caca9
Marco Pla <marco.pla@gmail.com>
2023-12-19 23:56:31 -0300

    feat (jest) : first jest commands added

--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,15 +1,17 @@
 import { somaHorasExtras, calculaDescontos } from '../index.js';
 
-test('Deve retornanr a soma das horas extras', () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
+describe('Testes dos cálculos de folha', () => {
+  it('Deve retornar a soma das horas extras', () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
 
-  expect(retornado).toBe(esperado);
-});
+    expect(retornado).toBe(esperado);
+  });
 
-test('Deve descontar o valor do salario', () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
+  it('Deve descontar o valor do salario', () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
 
-  expect(retornado).toBe(esperado);
+    expect(retornado).toBe(esperado);
+  });
 });
