a050e5e108e6961045df4e2d69f789a1d0c1117c
Marco Pla <marco.pla@gmail.com>
2023-12-28 23:54:02 -0300

    feat(carrinho) : test carrinho type and carrinho items

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -6,4 +6,14 @@ describe('Testes do carrinho', () => {
     const carrinho = new Carrinho();
     expect(carrinho.subtotal).toBeNull();
   });
+  it('Deve ter itens', () => {
+    const item = new Item('Banana', 2, 5);
+    const item2 = new Item('Maçã', 0.5, 1);
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+    expect(typeof carrinho).toBe('object');
+    expect(carrinho[0]).toBe(item);
+    expect(carrinho[1]).toBe(item2);
+  });
 });
