b61379d250137dbcae2e9d7eca8c873ffb6f5eee
Marco Pla <marco.pla@gmail.com>
2023-12-29 22:12:09 -0300

    feat(carrinho) toStrictEqual matcher added

--- carrinho/test/carrinho.test.js
+++ carrinho/test/carrinho.test.js
@@ -31,4 +31,22 @@ describe('Testes do carrinho', () => {
     }
     expect(englobaErroCarrinho).toThrowError('Carrinho de compras vazio');
   });
+  it('Deve adicionar o frete', () => {
+    const carrinho = new Carrinho();
+    carrinho.adicionaFrete(10);
+    expect(carrinho.frete).toBe(10);
+  });
+  it('Deve finalizar as compras', () => {
+    const item = new Item('Banana', 2, 5);
+    const item2 = new Item('Mel', 0.5, 1);
+    const carrinho = new Carrinho();
+    carrinho.adiciona(item);
+    carrinho.adiciona(item2);
+    carrinho.adicionaFrete(10);
+    expect(carrinho.finalizaCompra()).toStrictEqual({
+      subtotal: 10.5,
+      frete: 10,
+      total: 20.5,
+    });
+  });
 });
